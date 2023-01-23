31c89bb85bb9f985931780ca5f47b23091a8f83c
Jonathan <tatangamess@gmail.com>
2023-01-23 08:28:44 -0600

    desarrollo urbano v1

---
+++ app/Http/Controllers/Frontend/Login/LoginController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\Login;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    //
+}
